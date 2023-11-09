6bd5ef31fbb3ed5e79cb25c6c272ec97283c796e
Ivica Matic <ivica.matic@spatialdays.com>
2023-11-09 14:42:21 +0000

    fix: added export for exceptions

--- apim_subscriptions_manager/__init__.py
+++ apim_subscriptions_manager/__init__.py
@@ -1,2 +1,10 @@
 from .apim_subscriptions_manager import ApimSubscriptionsManager
-from .apim_subscriptions_manager import *
+from .apim_subscriptions_manager import (
+    APIMUserAlreadyExistsError,
+    APIMUserCreationError,
+    APIMUserNotFoundError,
+    APIMSubscriptionAlreadyExistsError,
+    APIMSubscriptionCreationError,
+    APIMSubscriptionNotFoundError,
+    APIMSubscriptionKeyRefreshError,
+)
